bf807f412cfe9ce4689ef22c84ef532d59d0f75d
lovefields <hho1993@gmail.com>
2019-09-16 17:01:21 +0900

    pop click event

--- common/js/editor.js
+++ common/js/editor.js
@@ -1,15 +1,29 @@
 class dragonEditor{
 	constructor(wrap = '.aditor_area', options = {}){
 		this.setting(wrap, options);
+		console.log(this.wrap);
 		this.bindingEvent();
 		this.closeLoding();
 	}
 
 	setting(wrap, options){
 		let $this = this;
-		$this.wrap = wrap === '' ? $this.getEl('.pop_bg') : $this.getEl(wrap);
-		$this.popBgArea = options.popBgArea === undefined ? $this.getEl('.pop_bg') : $this.getEl(options.popBgArea);
-		$this.lodingArea = options.lodingArea === undefined ? $this.getEl('.pop_loding') : $this.getEl(options.lodingArea);
+		$this.wrap = $this.checkOptionElement(wrap, '.aditor_area');
+
+
+
+
+		$this.popBtns = $this.checkOptionElement(options.popBtn, '.btn_pop', 'multi');
+		$this.popBgArea = $this.checkOptionElement(options.popBgArea, '.pop_bg');
+		$this.lodingArea = $this.checkOptionElement(options.lodingArea, '.pop_loding');
+	}
+
+	checkOptionElement(name, defaultName, type = 'single'){
+		if(type ===  'single'){
+			return name === undefined ? this.getEl(defaultName) : this.getEl(name);
+		}else{
+			return name === undefined ? this.getElList(defaultName) : this.getElList(name);
+		}
 	}
 
 	getEl(name){
@@ -22,8 +36,28 @@ class dragonEditor{
 		}
 	}
 
+	getElList(name){
+		let $el = document.querySelectorAll(name);
+
+		if($el.length < 1){
+			console.error('Can not find Element List : ' + name);
+		}else{
+			return $el;
+		}
+	}
+
 	bindingEvent(){
 		let $this = this;
+
+		$this.popBtns.forEach(function(btn){
+			btn.addEventListener('click', function(){
+				let target = this.dataset['target'];
+				let $el = $this.getEl(target);
+
+				this.classList.toggle('act');
+				$el.classList.toggle('act');
+			});
+		});
 
 		document.addEventListener('contextmenu', function(e){
 			e.preventDefault();
