f02a0bacf899381adf6f67923f1259fbc7554656
lovefields <hho1993@gmail.com>
2019-09-23 17:07:53 +0900

    add image add btn event

--- common/js/editor.js
+++ common/js/editor.js
@@ -8,21 +8,27 @@ class dragonEditor{
 
 	setting(wrap, options){
 		let $this = this;
+		$this.stickerListName = options.stickerListName === undefined ? '.pop_sticker' : options.stickerListName;
+		$this.imageIconId = options.imageIconId === undefined ? '#image' : options.imageIconId;
+
 		$this.wrap = $this.checkOptionElement(wrap, '.aditor_area');
 		$this.editorSection = $this.checkOptionElement(options.editorSection, '.editor_section');
 		$this.contentArea = $this.checkOptionElement(options.contentArea, '.content_area');
 		$this.contentAddList = $this.checkOptionElement(options.contentAddList, '.pop_content_list');
-		$this.stickerListName = options.stickerListName === undefined ? '.pop_sticker' : options.stickerListName;
+
+		$this.uploadForm = $this.checkOptionElement(options.uploadForm, '.file_uploader');
+		$this.fileInput = $this.checkOptionElement(options.fileInput, '.file_check');
 
 		$this.contentAddBtn = $this.checkOptionElement(options.contentAddBtn, '.btn_add_content', 'multi');
 		$this.viewBtn = $this.checkOptionElement(options.viewBtn, '.btn_mod');
 		$this.changeAreaBtn = $this.checkOptionElement(options.changeAreaBtn, '.btn_change_area');
+		
 		$this.popBtns = $this.checkOptionElement(options.popBtn, '.btn_pop', 'multi');
-
 		$this.popBgArea = $this.checkOptionElement(options.popBgArea, '.pop_bg');
 		$this.lodingArea = $this.checkOptionElement(options.lodingArea, '.pop_loding');
 
 		$this.HTMLTextBlock = '<p class="item item_text" contenteditable="true">[content]</p>';
+		$this.HTMLImageBtn = '<button class="btn" data-type="image"><svg viewbox="0 0 50 50" class="icon"><use class="path" xlink:href="[icon_id]" href="[icon_id]" /></svg>Add on image</button>';
 	}
 
 	checkOptionElement(name, defaultName, type = 'single'){
@@ -127,14 +133,17 @@ class dragonEditor{
 		$this.contentAddBtn.forEach(function($btn){
 			$btn.addEventListener('click', function(){
 				let type = this.dataset['value'];
+				let childCount = $this.contentArea.childElementCount;
 
 				$this.contentAddList.classList.remove('act');
 				switch(type){
 					case 'text':
-						$this.addTextBlock();
+						$this.addTextBlock(childCount);
 					break;
 					case 'image':
-						//$this.addImageBtn();
+						$this.addImageBtn(childCount);
+						$this.fileInput.setAttribute('accept', 'image/*');
+						$this.fileInput.click();
 					break;
 					case 'sticker':
 					break;
@@ -226,17 +235,20 @@ class dragonEditor{
 		this.lodingArea.classList.remove('act');
 	}
 
-	addTextBlock(content = ''){
+	addTextBlock(number, content = ''){
 		let $lastEl = this.contentArea.querySelector('.lastset');
-		let childCount = this.contentArea.childElementCount;
 		let html = this.HTMLTextBlock.replace('[content]', content);
-		let target = $lastEl === null ? this.contentArea.querySelector('*:nth-child('+ childCount +')') : $lastEl
+		let target = $lastEl === null ? this.contentArea.querySelector('*:nth-child('+ number +')') : $lastEl
 
 		target.insertAdjacentHTML('afterend', html);
 		target.nextElementSibling.focus();
 	}
 
-	addImageBtn(){
+	addImageBtn(number){
+		let $lastEl = this.contentArea.querySelector('.lastset');
+		let html = this.HTMLImageBtn.replace(/\[icon_id\]/g, this.imageIconId);
+		let target = $lastEl === null ? this.contentArea.querySelector('*:nth-child('+ number +')') : $lastEl
 
+		target.insertAdjacentHTML('afterend', html);
 	}
 }
