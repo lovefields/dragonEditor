6de280cd1b220990a8984112d1483146ecd0e307
lovefields <hho1993@gmail.com>
2020-01-16 18:28:46 +0900

    key event

--- common/js/deragnEditor/cursor.js
+++ common/js/deragnEditor/cursor.js
@@ -1,17 +1,18 @@
-export function clearCursor(){
+export function setCursor($node, position){
     let select = window.getSelection();
     let range = document.createRange();
 
-    console.log(select.getRangeAt(0));
-    if (select.empty) {  // Chrome
-        select.empty();
-    } else if (select.removeAllRanges) {  // Firefox
-        select.removeAllRanges();
-    }
-
     //console.log(select.focusNode, select.focusOffset);
-    //range.setStart(select.focusNode, select.focusOffset);
-    //range.collapse(true);
-    //select.removeAllRanges();
-    //select.addRange(range);
+    range.setStart($node, position);
+    range.collapse(true);
+    select.removeAllRanges();
+    select.addRange(range);
 }
+
+/*
+if (select.empty) {  // Chrome
+    select.empty();
+} else if (select.removeAllRanges) {  // Firefox
+    select.removeAllRanges();
+}
+*/
--- common/js/deragnEditor/keyboard.js
+++ common/js/deragnEditor/keyboard.js
@@ -1,6 +1,7 @@
 import { findParent } from './selector';
 import { addTextBlock } from './phrase';
 import { openOptionPop } from './option';
+import { setCursor } from './cursor';
 
 export async function keybroadControl(event){
     let selection = window.getSelection();
@@ -28,6 +29,9 @@ export async function keybroadControl(event){
                             if(count > 1){
                                 $activeEl.remove();
                             }
+                            let $target = $item.nextElementSibling;
+                            storage.activeElement = $target;
+                            openOptionPop($target.getBoundingClientRect(), 'text');
                         }else{
                             $activeEl.insertAdjacentHTML('afterend', storage.HTMLChildList.replace('[content]', ''));
                             setTimeout(() => {
@@ -57,12 +61,49 @@ export async function keybroadControl(event){
             }
         break;
         case key === 'Backspace'&& $activeEl !== false:
-            if(selection.focusOffset === 0 && selection.focusNode === $item.childNodes[0]){
-                if(textContent === ''){
-                    if(tagName === 'LI'){
+            if(selection.focusOffset === 0 && (selection.focusNode === $item.childNodes[0] || selection.focusNode === $item)){
+                event.preventDefault();
+                let $prevEl = $item.previousElementSibling;
+                let $prevElChild = $prevEl.childNodes;
+                let prevElChildCount = $prevElChild.length;
 
-                    }else if(tagName === 'P'){
+                if(textContent === ''){
+                    if(tagName === 'P' && $item.tagName !== 'BLOCKQUOTE'){
+                        let prevElTagName = $prevEl.tagName;
+                        if(prevElTagName === 'UL' || prevElTagName === 'OL'){
+                            let $list = $prevEl.children;
+                            let $listCount = $prevEl.childElementCount;
+                            let $target = $list[$listCount - 1];
+                            let $nodeList = $target.childNodes;
+                            let nodeListCount = $nodeList.length;
     
+                            if(nodeListCount > 0){
+                                let $target = $nodeList[nodeListCount -1];
+                                setCursor($target, $target.length);
+                            }else{
+                                setCursor($target, 0);
+                            }
+                            $item.remove();
+                            $prevEl.classList.add('lastset');
+
+                            if(prevElTagName === 'UL'){
+                                openOptionPop($prevEl.getBoundingClientRect(), 'list_u');
+                            }else if(prevElTagName === 'OL'){
+                                openOptionPop($prevEl.getBoundingClientRect(), 'list_o');
+                            }
+                        }else if(prevElTagName === 'P'){
+                            if(prevElChildCount > 0){
+                                let $target = $prevElChild[prevElChildCount -1];
+                                setCursor($target, $target.length);
+                            }else{
+                                setCursor($prevEl, 0);
+                            }
+                            $item.remove();
+                            $prevEl.classList.add('lastset');
+                            openOptionPop($prevEl.getBoundingClientRect(), 'text');
+                        }
+                    }else if(tagName === 'LI'){
+                        
                     }
                 }else{
                     console.log(selection.focusNode, $activeEl.childNodes[0]);
