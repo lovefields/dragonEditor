067947968c85643d0c27e296e81c4fa7ec98b972
Lovefields <hho1993@gmail.com>
2020-07-16 12:04:54 +0900

    rm common

--- common/js/deragnEditor/codeBlock.js
+++
@@ -1,7 +0,0 @@
-import { removeLastsetClass } from './element';
-
-export function addCodeBlock($target){
-    removeLastsetClass($target);
-    $target.insertAdjacentHTML('afterend', storage.HTMLCodeBlock);
-    $target.nextElementSibling.children[0].focus();
-}
