92b840c2d52defd9c583143136ac397feab0da47
Lovefields <hho1993@gmail.com>
2020-08-21 16:50:27 +0900

    set status & message

--- work-area/common/js/module/layout.js
+++ work-area/common/js/module/layout.js
@@ -1,6 +1,27 @@
-
-
-
 export function makeView(){
-    console.log("make view");
+    let view = "";
+
+    view += makecontentArea();
+    console.log(editorCondition.wrap);
+    console.log(editorCondition.blockMenu);
+/*
+    `
+<button class="btn btn_add_content" data-value="text" data-text="Text">
+<svg viewbox="0 0 50 50" class="icon">
+    <use class="path" xlink:href="#icon_text" href="#icon_text" />
+</svg>
+</button>
+`
+*/
+
+    
+    editorCondition.wrap.innerHTML = view;
+}
+
+function makecontentArea(){
+    return `<section class="editor-content" data-lang="${editorCondition.lang}"><p class="editor-item" contenteditable="true" data-type="text"></p></section>`;
+}
+
+function makeBlockMenu(data){
+
 }
