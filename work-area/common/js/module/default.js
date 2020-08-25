2b6a67e4b411b1469f5141efe77d947d5fc3dd41
Lovefields <hho1993@gmail.com>
2020-08-25 12:55:55 +0900

    layout & default event

--- work-area/common/js/module/default.js
+++ work-area/common/js/module/default.js
@@ -1,4 +1,4 @@
-export function typeCheckThrow(target, type){
+export function typeCheckThrow(target, type) {
     if (typeof type == "string") {
         if (typeof target != type) {
             throw `invaild type ${target} : ${type}`;
@@ -7,9 +7,9 @@ export function typeCheckThrow(target, type){
         throw `invaild type ${target} : ${type}`;
     }
     return target;
-};
+}
 
-export function typeCheckBoolean(target, type){
+export function typeCheckBoolean(target, type) {
     if (typeof type == "string") {
         if (typeof target != type) {
             return false;
@@ -18,4 +18,36 @@ export function typeCheckBoolean(target, type){
         return false;
     }
     return true;
-};
+}
+
+export function eventBinding($node, type, fn, _0 = typeCheckThrow($node, "object"), _1 = typeCheckThrow(type, "string"), _2 = typeCheckThrow(fn, "function")) {
+    if ($node.length > 0) {
+        $node.forEach(($item) => {
+            $item.addEventListener(type, fn);
+        });
+    } else {
+        $node.addEventListener(type, fn);
+    }
+}
+
+export function classControl($node, action, className, _0 = typeCheckThrow($node, "object"), _1 = typeCheckThrow(action, "string"), _2 = typeCheckThrow(className, "string")) {
+    if ($node.length > 0) {
+        $node.forEach(($item) => {
+            if (action == "add") {
+                $item.classList.add(className);
+            } else if (action == "remove") {
+                $item.classList.remove(className);
+            } else if (action == "toggle") {
+                $item.classList.toggle(className);
+            }
+        });
+    } else {
+        if (action == "add") {
+            $node.classList.add(className);
+        } else if (action == "remove") {
+            $node.classList.remove(className);
+        } else if (action == "toggle") {
+            $node.classList.toggle(className);
+        }
+    }
+}
--- work-area/common/js/module/layout.js
+++ work-area/common/js/module/layout.js
@@ -5,51 +5,67 @@ export function makeView() {
 
     view += `<div class="editor-menu-block">`;
     view += makeBlockMenu(editorCondition.defaultMenu);
+    //view += makeBlockMenu();
     view += `</div>`;
 
     view += `<div class="editor-menu-bottom">`;
     view += makeBottomMenu();
+    view += makeLanguagePop();
     view += `</div>`;
 
     editorCondition.wrap.innerHTML = view;
 }
 
 function makeContentArea(lang) {
-    return `<div class="editor-content" data-lang="${lang}"><p class="editor-item" contenteditable="true" data-type="text"></p></div>`;
+    return `<div class="editor-content js-content" data-lang="${lang}"><p class="editor-item js-item" contenteditable="true" data-type="text"></p></div>`;
 }
 
 function makeBlockMenu(data) {
     let html = "";
 
     for (const [key, value] of Object.entries(data)) {
-        html += `
-            <button class="editor-btn js-add-block" title="${value.text}" data-value="${key}">
-                <svg viewbox="0 0 64 64" class="editor-icon">
-                    <use class="path" xlink:href="${value.icon}" href="${value.icon}"></use>
-                </svg>
-                ${value.text}
-            </button>
-        `;
+        if (key === "emoticonBlock") {
+            html += `
+                <div class="editor-relative">
+                    <button class="editor-btn js-toggle-target" title="${value.text}" data-value="${key}" data-target=".editor-pop-emoticon">
+                        <svg viewbox="0 0 64 64" class="editor-icon">
+                            <use class="path" xlink:href="${value.icon}" href="${value.icon}"></use>
+                        </svg>
+                        ${value.text}
+                    </button>
+            `;
+            html += makeEmoticonPop();
+            html += `</div>`;
+        } else {
+            html += `
+                <button class="editor-btn js-add-block" title="${value.text}" data-value="${key}">
+                    <svg viewbox="0 0 64 64" class="editor-icon">
+                        <use class="path" xlink:href="${value.icon}" href="${value.icon}"></use>
+                    </svg>
+                    ${value.text}
+                </button>
+            `;
+        }
     }
 
     return html;
 }
 
-function makeBottomMenu(){
+function makeBottomMenu() {
     return `
-        <button class="editor-btn">
+        <button class="editor-btn js-toggle-target" data-target=".editor-pop-folder">
             <svg viewbox="0 0 64 64" class="editor-icon">
                 <use class="path" xlink:href="#icon-folder" href="#icon-folder"></use>
             </svg>
             Media
         </button>
-        <button class="editor-btn">
+        <button class="editor-btn js-toggle-target" data-target=".editor-pop-lang">
             <svg viewbox="0 0 64 64" class="editor-icon">
                 <use class="path" xlink:href="#icon-lang" href="#icon-lang"></use>
             </svg>
             Change language
         </button>
-        <button class="editor-btn editor-switch-device">
+        <button class="editor-btn editor-switch-device js-switch-device">
             <svg class="icon" viewbox="0 0 64 64">
                 <use class="path editor-pc" xlink:href="#icon_pc" href="#icon-pc"></use>
                 <use class="path editor-mobile" xlink:href="#icon_mobile" href="#icon-mobile"></use>
@@ -57,4 +73,29 @@ function makeBottomMenu(){
             Change view
         </button>
     `;
+}
+
+function makeLanguagePop() {
+    let html = `<div class="editor-pop-lang" data-length="${editorCondition.langCategory.length}">`;
+
+    editorCondition.langCategory.forEach((lang) => {
+        if(lang == editorCondition.lang){
+            html += `<button class="editor-btn-lang js-change-lang --act" data-value="${lang}">${lang.toUpperCase()}</button>`;
+        }else{
+            html += `<button class="editor-btn-lang js-change-lang" data-value="${lang}">${lang.toUpperCase()}</button>`;
+        }
+    });
+
+    html += `</div>`;
+
+    return html;
+}
+
+function makeEmoticonPop() {
+    let html = `<div class="editor-pop-emoticon">`;
+
+
+    html += `</div>`;
+
+    return html;
 }
