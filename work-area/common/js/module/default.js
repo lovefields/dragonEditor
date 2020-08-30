d5bc39748f8daca70c06aac7509b6af2d0aee841
Lovefields <hho1993@gmail.com>
2020-08-31 08:43:34 +0900

    scroll event

--- work-area/common/js/module/default.js
+++ work-area/common/js/module/default.js
@@ -30,15 +30,15 @@ export function eventBinding($node, type, fn, useCapture = false, _0 = typeCheck
     }
 }
 
-export function removeEvent($node, type, fn, _0 = typeCheckThrow($node, "object"), _1 = typeCheckThrow(type, "string"), _2 = typeCheckThrow(fn, "function")) {
-    if ($node.length > 0) {
-        $node.forEach(($item) => {
-            $item.removeEventListener(type, fn, true);
-        });
-    } else {
-        $node.removeEventListener(type, fn, true);
-    }
-}
+// export function removeEvent($node, type, fn, _0 = typeCheckThrow($node, "object"), _1 = typeCheckThrow(type, "string"), _2 = typeCheckThrow(fn, "function")) {
+//     if ($node.length > 0) {
+//         $node.forEach(($item) => {
+//             $item.removeEventListener(type, fn, true);
+//         });
+//     } else {
+//         $node.removeEventListener(type, fn, true);
+//     }
+// }
 
 export function classControl($node, action, className, _0 = typeCheckThrow($node, "object"), _1 = typeCheckThrow(action, "string"), _2 = typeCheckThrow(className, "string")) {
     if ($node.length > 0) {
@@ -60,4 +60,23 @@ export function classControl($node, action, className, _0 = typeCheckThrow($node
             $node.classList.toggle(className);
         }
     }
+}
+
+export function hasClass($node, className, _0 = typeCheckThrow($node, Node), _1 = typeCheckThrow(className, "string")) {
+    let value = false;
+    let nameList = className.split(".");
+
+    nameList.some((name) => {
+        if(name !== ""){
+            if($node.classList.contains(name)){
+                value = true;
+            }else{
+                value = false;
+            }
+
+            return value == false;
+        }
+    });
+
+    return value;
 }
