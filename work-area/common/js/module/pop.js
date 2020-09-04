49507cc45455766730063163e0f42e70d73cdbd8
Lovefields <hho1993@gmail.com>
2020-09-04 14:53:01 +0900

    set link pop & youtube item

--- work-area/common/js/module/pop.js
+++ work-area/common/js/module/pop.js
@@ -1,20 +1,40 @@
 const { typeCheckThrow, classControl } = require("./default");
-const { getElement } = require("./selector");
+const { getElement, getChild } = require("./selector");
 
-export function openPop(type, _0 = typeCheckThrow(type, "string")) {
+export function openPop(type, $node, _0 = typeCheckThrow(type, "string"), _1 = typeCheckThrow($node, Node)) {
     let trigger = getElement(".djs-trigger");
+    let offset = $node.getBoundingClientRect();
 
     classControl(trigger, "remove", "--act");
     switch (type) {
         case "linkboxBlock":
+            openLinkPop("linkbox", offset);
             break;
         case "emoticonBlock":
             classControl(editorCondition.popEmoticon, "toggle", "--act");
             break;
         case "youtubeBlock":
+            openLinkPop("youtube", offset);
             break;
         case "codepenBlock":
+            openLinkPop("codepen", offset);
             break;
     }
-    console.log(type);
+}
+
+function openLinkPop(type, option = {}, _0 = typeCheckThrow(type, "string"), _1 = typeCheckThrow(option, "object")){
+    let $btn = getChild(editorCondition.popLinkbox, ".djs-btn", false);
+    let right = (editorCondition.windowWidth - option.right) + (option.width + 10);
+
+    if(type == "word"){
+        $btn.dataset["value"] = type;
+        editorCondition.popLinkbox.dataset["type"] = option.type;
+        $btn.dataset["type"] = option.type;
+    }else{
+        $btn.dataset["value"] = type;
+        editorCondition.popLinkbox.style.top = `${option.top}px`;
+        editorCondition.popLinkbox.style.right = `${right}px`;
+    }
+
+    classControl(editorCondition.popLinkbox, "toggle", "--act");
 }
