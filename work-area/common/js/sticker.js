2ddfdb7cc80d8a82639450fb87f6114c70afa04e
Lovefields <hho1993@gmail.com>
2020-09-07 16:57:26 +0900

    delete file

--- work-area/common/js/sticker.js
+++
@@ -1,7 +0,0 @@
-import { removeLastsetClass } from './element';
-
-export function addSticker($target, sticker){
-    removeLastsetClass($target);
-    let html = storage.HTMLsticker.replace('[el]', sticker.innerHTML);
-    $target.insertAdjacentHTML('afterend', html);
-}
