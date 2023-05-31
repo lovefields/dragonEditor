b99f2f42f43355e068cc4c887742750eb18b4734
lovefields <hho1993@gmail.com>
2023-05-31 17:34:45 +0900

    test: build test

--- old/resources/js/module/option.js
+++
@@ -1,9 +0,0 @@
-const { typeCheckThrow } = require("./default");
-
-export function setTextItemOption($node, option, _0 = typeCheckThrow($node, "node"), _1 = typeCheckThrow(option, "object")) {
-    for (const [key, value] of Object.entries(option)) {
-        if (value != "") {
-            $node.dataset[key] = value;
-        }
-    }
-}
