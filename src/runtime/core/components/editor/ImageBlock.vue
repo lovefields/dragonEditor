59aaae4217aa81c5b7adaea89568e05393717e89
lovefields <hho1993@gmail.com>
2023-06-05 14:35:40 +0900

    feat[text]: backspace event

--- src/runtime/core/components/editor/ImageBlock.vue
+++ src/runtime/core/components/editor/ImageBlock.vue
@@ -1,16 +1,13 @@
 <template>
-    <div class="d-image-block" :class="data.classList">
+    <div class="d-image-block" :class="data.classList" ref="$block">
         <div class="d-image-area">
             <button class="d-btn-size-left"></button>
             <button class="d-btn-size-right"></button>
 
-            <template v-if="data.webp">
-
-            </template>
+            <template v-if="data.webp"> </template>
 
             <template v-else>
-                <img class="d-img" :src="data.src" :width="data.width" :height="data.height" :alt="data.caption"
-                    loading="lazy">
+                <img class="d-img" :src="data.src" :width="data.width" :height="data.height" :alt="data.caption" loading="lazy" />
             </template>
         </div>
         <p class="d-caption" v-html="data.caption" @keydown="textKeyboardEvent" ref="$caption" contenteditable></p>
@@ -19,9 +16,10 @@
 
 <script setup lang="ts">
 import { ref, unref } from "#imports";
-import { keyboardEvent } from "../../utils/index";
+import { keyboardEvent, pasteText } from "../../utils/index";
 import { imageBlock, cursorSelection } from "../../../../types/index";
 
+const $block = ref();
 const $caption = ref();
 const data = ref<imageBlock>({
     type: "",
@@ -33,25 +31,52 @@ const data = ref<imageBlock>({
     webp: false,
     caption: "",
 });
-const props = defineProps<{ modelValue: imageBlock, cursorData: cursorSelection }>();
+const props = defineProps<{ modelValue: imageBlock; cursorData: cursorSelection }>();
 const emit = defineEmits<{
     (e: "update:modelValue", modelValue: imageBlock): void;
     (e: "addBlock", name: string): void;
+    (e: "deleteBlockLocal", index?: number): void;
 }>();
 
 data.value = unref(props.modelValue) as imageBlock;
 
 // 키보드 이벤트 할당
 function textKeyboardEvent(e: KeyboardEvent) {
-    keyboardEvent("image", e, emit);
+    keyboardEvent("image", e, emit, updateBlockData);
 }
 
-// 외부용 함수
-function updateBlockData() { // 데이터 정규화 및 검수
+/**
+ * 외부용 함수
+ */
+function focus() {
+    $caption.value.focus();
+}
+
+// 블럭 위치 주기
+function getBoundingClientRect() {
+    return $block.value.parentNode.getBoundingClientRect();
+}
+
+// 타입 전달
+function getType() {
+    return data.value.type;
+}
+
+// 붙여넣기 이벤트
+function pasteEvent(text: string) {
+    pasteText("text", text);
+}
+
+// 데이터 정규화 및 검수
+function updateBlockData() {
     emit("update:modelValue", data.value);
 }
 
 defineExpose({
     updateBlockData,
+    getType,
+    focus,
+    pasteEvent,
+    getBoundingClientRect,
 });
 </script>
