9ecc4eb9bd03361899ff867953f9180083fbceb4
lovefields <hho1993@gmail.com>
2024-08-08 13:45:13 +0900

    feat: default setting

--- src/runtime/components/DragonEditor.vue
+++ src/runtime/components/DragonEditor.vue
@@ -1,3 +1,24 @@
 <template>
-    <div class="dragon-editor"> eidtor </div>
+    <div class="dragon-editor">
+        <div v-if="props.useMenuBar === true" class="d-menu-area">menu bar!</div>
+        <div class="d-body"> </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { ref, unref, onMounted } from "#imports";
+
+const props = defineProps({
+    useMenuBar: {
+        type: Boolean,
+        requiard: false,
+        default: () => true,
+    },
+});
+
+console.log(props.useMenuBar);
+</script>
+
+<style lang="scss">
+@import "../scss/editor.scss";
+</style>
