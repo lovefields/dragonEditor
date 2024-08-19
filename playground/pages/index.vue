cd21319b6d76361a00fa31e2a6e79691bfe3f225
lovefield <hho1993@gmail.com>
2024-08-19 16:42:56 +0900

    feat: viewer & data controller

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -3,16 +3,34 @@
         editor!
         <div class="editor-area">
             <ClientOnly>
-                <DragonEditor />
+                <DragonEditor ref="$editor" />
             </ClientOnly>
         </div>
+        <button @click="getContent">get data</button>
+        <button @click="setContent">set data</button>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from "#imports";
 
-const test = ref<MyModuleNitroRulesss>({});
+const $editor = ref();
+
+function getContent() {
+    console.log("data", $editor.value.getContentData());
+}
+
+function setContent() {
+    $editor.value.setContentData([
+        { type: "text", textContent: "123" },
+        { type: "text", textContent: "123" },
+        { type: "heading", level: 1, id: "Sgdaow", textContent: "123" },
+        { type: "heading", level: 2, id: "zlfm8i", textContent: "123" },
+        { type: "heading", level: 3, id: "D0ZRSP", textContent: "123" },
+        { type: "ul", child: ["123"] },
+        { type: "ol", pattern: "1", child: ["123"] },
+    ]);
+}
 </script>
 
 <style lang="scss">
