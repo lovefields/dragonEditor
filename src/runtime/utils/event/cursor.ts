e32a39ed6ff3b0c984a9f29bdb246f6dc6ea0069
lovefields <hho1993@gmail.com>
2025-04-07 21:27:23 +0900

    refactor: block data

--- src/runtime/utils/event/cursor.ts
+++ src/runtime/utils/event/cursor.ts
@@ -37,3 +37,72 @@ export function _setCursor($target: Node, startIdx: number) {
     selection.removeAllRanges();
     selection.addRange(range);
 }
+
+// 엘리먼트 기준 커서 데이터 정렬
+export function _sortingCursorDataOnElement(cursorData: DEditorCursor, $element: HTMLElement): DEArrangeCursorData {
+    const childList = $element.childNodes;
+    let startNode = cursorData.startNode;
+    let startIdx = -1;
+    let startOffset = cursorData.startOffset;
+    let endNode = cursorData.endNode;
+    let endIdx = -1;
+    let endOffset = cursorData.endOffset;
+
+    if (startNode.constructor.name === "Text") {
+        if (startNode.parentElement !== $element) {
+            startNode = startNode.parentElement as HTMLElement;
+        }
+    }
+    if (endNode.constructor.name === "Text") {
+        if (endNode.parentElement !== $element) {
+            endNode = endNode.parentElement as HTMLElement;
+        }
+    }
+
+    for (let i = 0; childList.length > i; i += 1) {
+        if (startIdx !== -1 && endIdx !== -1) {
+            break;
+        }
+
+        if (startNode === childList[i]) {
+            startIdx = i;
+        }
+
+        if (endNode === childList[i]) {
+            endIdx = i;
+        }
+    }
+
+    if (startIdx === endIdx) {
+        // 같은 노드인 경우
+
+        if (cursorData.startOffset > cursorData.endOffset) {
+            startOffset = cursorData.endOffset;
+            endOffset = cursorData.startOffset;
+        }
+    } else {
+        // 다른 노드인 경우
+        if (startIdx > endIdx) {
+            const originStartNode = startNode;
+            const originStartIdx = startIdx;
+            const originEndNode = endNode;
+            const originEndIdx = endIdx;
+
+            startNode = originEndNode;
+            startIdx = originEndIdx;
+            startOffset = cursorData.endOffset;
+            endNode = originStartNode;
+            endIdx = originStartIdx;
+            endOffset = cursorData.startOffset;
+        }
+    }
+
+    return {
+        startNode: startNode,
+        startNodeIdx: startIdx,
+        startOffset: startOffset,
+        endNode: endNode,
+        endNodeIdx: endIdx,
+        endOffset: endOffset,
+    };
+}
